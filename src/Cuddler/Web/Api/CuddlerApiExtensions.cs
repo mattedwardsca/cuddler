54601928c85ace4931f54700ded74f2b9696002c
Matt Edwards <matt@enduser.ca>
2023-01-11 06:32:31 -0700

    t

--- src/Cuddler/Web/Api/CuddlerApiExtensions.cs
+++ src/Cuddler/Web/Api/CuddlerApiExtensions.cs
@@ -6,6 +6,8 @@ namespace Cuddler.Web.Api;
 
 public class CuddlerApi
 {
+    public static Mock Mock = new();
+
     public static CuddlerUri<T> Grid<T>(Expression<Func<T, Task<IActionResult>>> func) where T : class, IApiController
     {
         return new CuddlerUri<T>().Endpoint(func);
@@ -21,27 +23,24 @@ public class CuddlerApi
         return new CuddlerUri<T>().Endpoint(func)
                                   .ToFormModel();
     }
-
-    public static Mock Mock = new();
-
 }
 
 public class Mock
 {
+    public static string String => Object<string>();
+
+    public static bool Bool => Object<bool>();
+
+    public static int Int => Object<int>();
+
+    public static decimal Decimal => Object<decimal>();
+
+    public static double Double => Object<double>();
+
+    public static DateTime DateTime => Object<DateTime>();
+
     public static TModel Object<TModel>()
     {
         return Activator.CreateInstance<TModel>();
     }
-
-    public string String => Object<string>();
-    
-    public bool Bool => Object<bool>();
-    
-    public int Int => Object<int>();
-    
-    public decimal Decimal => Object<decimal>();
-    
-    public double Double => Object<double>();
-    
-    public DateTime DateTime => Object<DateTime>();
 }
